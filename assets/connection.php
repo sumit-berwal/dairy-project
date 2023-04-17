035f9b581dbb53141c8b55a9a06c9664c34eec35
Sumit Berwal <sumitberwal85@gmail.com>
2023-04-17 12:46:50 +0530

    sdfj

---
+++ assets/connection.php
@@ -0,0 +1,14 @@
+<?php
+// $server = "localhost";
+// $username= "root";
+// $password = "sumit";
+// $dbname = "mypractice";
+
+// $con = mysqli_connect("$server","$username","$pasword", "$dbname");
+
+// if($con){
+//     echo "connection successful";
+// }
+
+
+echo "This is connection file";
